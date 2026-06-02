483ebda8757890436d83820d529873e4f1e73886
krd16 <230078@iessanalberto.com>
2026-06-02 17:19:08 +0200

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module org.example.proyectofinaleedd {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens org.example.proyectofinaleedd to javafx.fxml;
+    exports org.example.proyectofinaleedd;
+}
